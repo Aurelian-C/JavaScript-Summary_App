eb6fa68a41874189c9e6a457a0d199efc1109c54
Aurelian-C <aurelian_reghin@yahoo.com>
2022-03-16 22:09:23 +0200

    Update application

--- src/js/model/modelDataStorage.js
+++ src/js/model/modelDataStorage.js
@@ -5429,11 +5429,21 @@ const modulesAndTooling = {
         },
       ],
       sectionExplanations: [
-        `Parcel is just another build tool which is also on NPM and you need to use NPM to install it.`,
-        `A devDependency is like a tool that we need to build our application but it's not a dependency that we actually include in our code, so it's simply a tool and so that's why it's called a devDependency because we can use it to develop our project and so therefore it appears in a new field in our package.json file.`,
-        `We use Parcel in the terminal because Parcel is basically just another command line interface. In order to still be able to use Parcel in the terminal, we have two options: so we can use something called NPX or we can use NPM scripts.`,
-        `With Parcel you don't need to specify the entire path to a library. In all module bundlers there's no need for specifying the entire path to any module, it’s enough to write the name of the package/module and Parcel will then automatically find the path to the package/module and will import it like when we specified the entire path without us having to manually type the entire path to the package/module.`,
-        `Parcel works with all kinds of assets, even with HTML, CSS, SASS files, images, svg and of course also all kinds of modules, so not only ES6 modules but this is also going to work with CommonJS modules.`,
+        `Install and configure Parcel:
+        <br>1. Install ' <b>npm i parcel --save-dev</b> '
+        <br>2. Acces package.json → write <b>"start": "parcel index.html"</b> in "script" object → in terminal write ' <b>npm run start</b> ' or just simple ' <b>npm start</b> '
+        <br>3. Write <b>"build": "parcel build index.html –dist-dir ./dist"</b> in "script" object → in terminal write ' <b>npm run build</b> '
+        <br>If you want to uninstall parcel then you write in the terminal <b>npm un parcel</b>
+        <br>In Parcel 2, before you run the build script, you need to modify "main": "index.js" in "default": "index.js".
+
+      `,
+        `<b>Summary</b>:
+      <br>➣ Parcel is just another build tool which is also on NPM and you need to use NPM to install it.
+      <br>➣ A devDependency is like a tool that we need to build our application but it's not a dependency that we actually include in our code, so it's simply a tool and so that's why it's called a devDependency because we can use it to develop our project and so therefore it appears in a new field in our package.json file.
+      <br>➣ We use Parcel in the terminal because Parcel is basically just another command line interface. In order to still be able to use Parcel in the terminal, we have two options: so we can use something called NPX or we can use NPM scripts.
+      <br>➣ With Parcel you don't need to specify the entire path to a library. In all module bundlers there's no need for specifying the entire path to any module, it’s enough to write the name of the package/module and Parcel will then automatically find the path to the package/module and will import it like when we specified the entire path without us having to manually type the entire path to the package/module.
+      <br>➣ Parcel works with all kinds of assets, even with HTML, CSS, SASS files, images, svg and of course also all kinds of modules, so not only ES6 modules but this is also going to work with CommonJS modules.
+     `,
       ],
       sectionArticles: [
         {
